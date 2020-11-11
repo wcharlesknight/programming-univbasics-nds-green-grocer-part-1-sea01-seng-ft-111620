43e4535d014ee44bbdc9a2bc5571e9e71fd1b1bc
Charlie Knight <williamchknight@gmail.com>
2020-11-11 18:01:08 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,14 +19,11 @@ def consolidate_cart(cart)
  cart.each do |item_list|
    item_list.each do |item, value|
      new_cart[item] ||= value
-     if new_cart[item][:count] = True
-       new_cart[item][:count] +=1
-     else
-       new_cart[item][:count] = 1
+     
      end 
    end
  end
- new_cart
+
 end 
 
 
