aee1afc65ccd9a8449bc6446dd6c35893fc85038
Charlie Knight <williamchknight@gmail.com>
2020-11-11 18:42:33 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,7 +19,7 @@ def consolidate_cart(cart)
   index = 0
   while index < cart.length do
     new_cart_item = find_item_by_name_in_collection(cart[index][:item], new_cart)
-    if new_cart_item != nil
+    if new_cart_item
       new_cart_item[:count] += 1
     else
       cart[index][:count] = 1
