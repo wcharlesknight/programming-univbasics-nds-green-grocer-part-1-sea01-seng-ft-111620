bb4623bb8ef75eb20d304fd022fc76cbdc451be3
Charlie Knight <williamchknight@gmail.com>
2020-11-11 18:16:48 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -15,15 +15,10 @@ end
 
 
 def consolidate_cart(cart) 
-  new_cart = {} 
-  cart.each do |items_array| 
-    items_array.each do |item, attribute_hash| 
-      new_cart[item] ||= attribute_hash 
-      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
-      new_cart[item][:count] = 1 
-  end 
-end 
-new_cart.to_a 
+  new_cart = []
+  cart.each do |items|
+    binding.pry
+  end  
 end
 
 
