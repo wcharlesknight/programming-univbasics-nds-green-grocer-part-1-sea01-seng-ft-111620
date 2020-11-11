d0914f701362410d39289dc44b431004b3c83d73
Charlie Knight <williamchknight@gmail.com>
2020-11-11 18:22:03 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -17,8 +17,14 @@ end
 def consolidate_cart(cart) 
   new_cart = []
   cart.each do |items|
-    binding.pry
-  end  
+    if new_cart == []
+      items[:count] = 1 
+      new_cart << items
+    if new_cart == items
+      new_cart.merge({count +=1 })
+    end
+  end
+  binding.pry 
 end
 
 
