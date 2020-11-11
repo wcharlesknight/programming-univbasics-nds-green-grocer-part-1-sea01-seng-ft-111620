1c98ff8cce879eefb5047e685a461a5c361024e1
Charlie Knight <williamchknight@gmail.com>
2020-11-11 17:23:41 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -15,10 +15,19 @@ end
 
 
 def consolidate_cart(cart)
- cart.each_with_object([]) do |(item) , new|
-  binding.pry
- end   
+ final = Hash.new 0 
+ count = :count
+ item.each do |hash|
+  hash.each do |food, description|
+   if final.has_key?(food) == false
+    final[food] = description
+    final[food][count] = 1
+   elsif final.has_key?(food)
+    final[food][:count] +=1
+   end
+  end
 end
+
 
 
   
